a422b826906802a7a87cea570a173e1860e5f39e
yfliuyanyan <liouyanyan9@126.com>
2025-02-28 14:02:56 +0800

    Update config.rs to customize server

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.65.189.185"];
+pub const RS_PUB_KEY: &str = "OGS3Niry8LBYwR4LMfMmndwhsg5B0TmD2IUkyQrZ+Yg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
