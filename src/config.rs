b8336a737b2c63dd17ac540b0df1319da42a49cd
yfliuyanyan <liouyanyan9@126.com>
2025-04-10 17:11:46 +0800

    Update config.rs to customize server

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["10.65.189.185"];
-pub const RS_PUB_KEY: &str = "OGS3Niry8LBYwR4LMfMmndwhsg5B0TmD2IUkyQrZ+Yg=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.9.139.25"];
+pub const RS_PUB_KEY: &str = "uUw1gls5TOTW1v6BkdobtWf3iRSGpcn9ibcJc3DESqI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
